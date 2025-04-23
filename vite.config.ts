9b131be1ede57752d1d72be6f58ab47bda4a446c
aliehs74 <fe.afra@gmail.com>
2025-04-23 15:40:22 +0330

    config bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig  } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,22 @@ export default defineConfig({
     environment: 'happy-dom',
     setupFiles: ['./setup.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}']
+  },
+  // اضافه کردن تنظیمات بیلد برای حالت لایبرری
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'), // یا 'src/main.tsx' اگر از TS استفاده می‌کنید
+      name: 'Riseco',
+      fileName: (format) => `riseco.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
   }
 })
